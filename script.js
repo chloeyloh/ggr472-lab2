9e0c52c961902ed00efebf73ee350bce45d3b249
chloeyloh <254973343+chloeyloh@users.noreply.github.com>
2026-02-10 14:51:43 -0500

    Updated index.html and style.css with new content for the web map application. The index.html file now includes a title and comments, while the style.css file has been updated with new styles for the map container. Changes to be committed include modifications to index.html and script.js, while style.css has changes that are not staged for commit.

--- script.js
+++ script.js
@@ -5,28 +5,25 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiY2hsb2Vsb2giLCJhIjoiY21sZ3UwZWYzMDIyazNocHNmc
 const map = new mapboxgl.Map({
     container: 'my-map', // container ID
     style: 'mapbox://styles/chloeloh/cmlguusrw00jx01qq87ehggny', // style URL
-    center: [-79.3832, 43.6532], // starting position [lng, lat]
-    zoom: 12 // starting zoom
+    center: [-79.3832, 43.6532], // Starting position on Downtown Toronto
+    zoom: 12 // starting zoom position
 });
 
 map.on('load', () => {
     // Adding my GeoJSON source to the map using the raw link from my Lab 1's GitHub repository
-    map.addSource('lab1map', {
+    map.addSource('film-spots-data', {
         type: 'geojson',
         data: 'https://raw.githubusercontent.com/chloeyloh/ggr472-lab1/refs/heads/main/lab1map.geojson'
     });
-
-
 
     // Adding the layers to the map to visualize the GeoJSON data with specified styling
     map.addLayer({
         id: 'American Psycho',
         type: 'circle',
-        source: 'lab1map',
+        source: 'film-spots-data',
         paint: {
             'circle-radius': 5,
-            'circle-color': '#bcbf00ff'
-        }
-    });
-
-
+            'circle-color': '#bcbf00ff'        
+        }   
+        });
+});
